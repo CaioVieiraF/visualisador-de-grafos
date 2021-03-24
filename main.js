e1d3190c7668dcae4d7157ab3680935689cb9a50
caio <wabros@protonmail.com>
2021-03-23 23:34:18 -0300

    implemented labeling

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 const canvas = document.querySelector('canvas#canvas');
 const screen = canvas.getContext('2d');
-const radius = 3;
+const radius = 10;
 let dotsPos = [];
 let lastClicked = [];
 let drawLine = false;
@@ -12,20 +11,23 @@ function main() {
         const rect = canvas.getBoundingClientRect()
         const x = e.clientX - rect.left
         const y = e.clientY - rect.top
-        if (e.target === canvas && e.buttons === 1){
+        if (e.target === canvas && e.buttons === 1) {
             let clickedOnDot = {
-                condition : false,
-                x : 0,
-                y : 0
+                condition: false,
+                x: 0,
+                y: 0
             };
-            for(let each = 0; each < dotsPos.length; each++) {
+            for (let each = 0; each < dotsPos.length; each++) {
                 const dotX = dotsPos[each][0];
                 const dotY = dotsPos[each][1];
                 const colisionRadius = radius + 2;
-                if (x > dotX-colisionRadius && x < dotX+colisionRadius && y > dotY-colisionRadius && y < dotY+colisionRadius) {
+                
+                if (x > dotX - colisionRadius && x < dotX + colisionRadius && y > dotY - colisionRadius && y < dotY + colisionRadius) {
                     clickedOnDot.condition = true;
                     clickedOnDot.x = dotX;
                     clickedOnDot.y = dotY;
+                    dotsPos[each][2] = window.prompt("Novo nome", dotsPos[each][2]);
+                    reRender();
                 }
             }
 
@@ -39,36 +41,34 @@ function main() {
                 );
                 lastClicked = [clickedOnDot.x, clickedOnDot.y];
             } else {
-                addDot(x, y);
+                const label = "Ponto " + dotsPos.length;
+                addDot(x, y, label);
             }
         }
     });
 
-    function addRandomDot(){
+    function addRandomDot() {
+        const label = "Ponto " + dotsPos.length;
         const padding = 5;
         addDot(
-            Math.random() * (canvas.width-padding - padding) + padding,
-            Math.random() * (canvas.height-padding - padding) + padding
+            Math.random() * (canvas.width - padding - padding) + padding,
+            Math.random() * (canvas.height - padding - padding) + padding,
+            label
         );
     }
 
 
-    function clearScreen(){
+    function clearScreen() {
         screen.clearRect(0, 0, canvas.width, canvas.height);
         dotsPos = [];
     }
 
     function changeDotLineMode(x) {
         drawLine = !drawLine;
-        if (drawLine) {
-            x.innerHTML = 'Dot';
-        } else {
-            x.innerHTML = 'Line';
-        }
     }
 
-    function undoMovement(){
-
+    function undoMovement() {
+        window.alert('Not implemented, yet');
     }
 
     themeOptions();
@@ -78,11 +78,14 @@ function main() {
         'Line/Dot': changeDotLineMode,
         Undo: undoMovement,
         Clear: clearScreen,
-    }
+    };
+
     const optionsBar = document.querySelector('div.bar');
     for (let buttonOption in buttonOptions) {
         const newButton = document.createElement("button");
-        newButton.onclick = () => {buttonOptions[buttonOption](this)};
+        newButton.onclick = () => {
+            buttonOptions[buttonOption]();
+        };
         newButton.innerHTML = buttonOption;
         optionsBar.appendChild(newButton);
     }
@@ -105,7 +108,7 @@ function themeOptions() {
         }
     }
     const container = document.querySelector('div.theme');
-    for(let theme in themes){
+    for (let theme in themes) {
         const button = document.createElement("button");
         button.onclick = () => {
             changeTheme(
@@ -125,25 +128,34 @@ function addLine(from, to) {
     screen.stroke();
 }
 
-function addDot(xPos, yPos){
-    screen.save();
-    dotsPos.push([xPos, yPos]);
+function addDot(xPos, yPos, label, render=true) {
+    if (render) dotsPos.push([xPos, yPos, label]);
     screen.fillStyle = 'black';
     screen.beginPath();
-    screen.arc(xPos, yPos, radius, 0, 2*Math.PI);
+    screen.arc(xPos, yPos, radius, 0, 2 * Math.PI);
     screen.fill();
     screen.stroke();
+    screen.font = "10px Arial";
+    screen.textAlign = "center";
+    screen.fillText(label, xPos, yPos + radius + 12);
     if (dotsPos.length >= 2 && drawLine) {
-        addLine(lastClicked, dotsPos[dotsPos.length-1]);
+        addLine(lastClicked, dotsPos[dotsPos.length - 1]);
     }
     lastClicked = [xPos, yPos];
 }
 
-function changeTheme(primary, secondary, txtColor='#adb5b8') {
+function reRender(){
+    screen.clearRect(0, 0, canvas.width, canvas.height);
+    for (let dot = 0; dot < dotsPos.length; dot++){
+        addDot(dotsPos[dot][0], dotsPos[dot][1], dotsPos[dot][2], false);
+    }
+}
+
+function changeTheme(primary, secondary, txtColor = '#adb5b8') {
     document.body.style.backgroundColor = primary;
     const buttons = document.querySelector('div.bar').children;
     for (let button in buttons) {
-        if (button == Number(button)){
+        if (button == Number(button)) {
             buttons[button].style.backgroundColor = secondary;
             buttons[button].style.color = txtColor;
         }
